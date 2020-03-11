f3983fefce6481842e9787ede3c5726ef0b3f77f
Euan Widjaja <ewi45@uclive.ac.nz>
2020-03-11 18:50:13 +1300

    Add user and petition mvc files with some functionality implemented

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -9,45 +9,51 @@ const User = require('../models/user.model');
 // The server will compare this to the value it has stored in the database
 // to authenticate that request.",
 exports.login = async function (req, res) {
+
+    let emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+
+    if (typeof req.body.email === 'undefined') {
+        res.statusMessage = 'Bad Request: Data should have required property email';
+        res.status(400)
+            .send('Data should have required property email');
+        return
+    } else if (typeof req.body.password === 'undefined') {
+        res.statusMessage = 'Bad Request: Data should have required property password';
+        res.status(400)
+            .send('Data should have required property password');
+        return
+    }
+
+    let email = req.body.email.toString();
+    let password = req.body.password.toString();
+
+    if (email.length < 1) {
+        res.statusMessage = 'Bad Request: data.email should NOT be shorter than 1 characters';
+        res.status(400)
+            .send('data.email should NOT be shorter than 1 characters');
+        return
+    } else if (password.length < 1) {
+        res.statusMessage = 'Bad Request: data.password should NOT be shorter than 1 characters';
+        res.status(400)
+            .send('data.password should NOT be shorter than 1 characters');
+        return
+    } else if (emailRegex.test(email) === false) {
+        res.statusMessage = "Bad Request: data.email should match format \"email\"";
+        res.status(400)
+            .send("Bad Request: data.email should match format \"email\"");
+        return
+    }
+
     try {
-        let emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
-
-        if (typeof req.body.email === 'undefined') {
-            res.statusMessage = 'Bad Request: Data should have required property email';
+        const result = await User.login(email, password);
+        if (typeof result === 'undefined') {
+            res.statusMessage = "Bad Request: Invalid email/password supplied";
             res.status(400)
-                .send('Data should have required property email');
-        } else if (typeof req.body.password === 'undefined') {
-            res.statusMessage = 'Bad Request: Data should have required property password';
-            res.status(400)
-                .send('Data should have required property password');
+                .send('Invalid email/password supplied');
         } else {
-            let email = req.body.email.toString();
-            let password = req.body.password.toString();
-
-            if (email.length < 1) {
-                res.statusMessage = 'Bad Request: data.email should NOT be shorter than 1 characters';
-                res.status(400)
-                    .send('data.email should NOT be shorter than 1 characters');
-            } else if (password.length < 1) {
-                res.statusMessage = 'Bad Request: data.password should NOT be shorter than 1 characters';
-                res.status(400)
-                    .send('data.password should NOT be shorter than 1 characters');
-            } else if (emailRegex.test(email) === false) {
-                res.statusMessage = "Bad Request: data.email should match format \"email\"";
-                res.status(400)
-                    .send("Bad Request: data.email should match format \"email\"");
-            } else {
-                const result = await User.login(email, password);
-                if (typeof result === 'undefined') {
-                    res.statusMessage = "Bad Request: Invalid email/password supplied";
-                    res.status(400)
-                        .send('Invalid email/password supplied');
-                } else {
-                    res.statusMessage = "OK";
-                    res.status(200)
-                        .send(result);
-                }
-            }
+            res.statusMessage = "OK";
+            res.status(200)
+                .send(result);
         }
     } catch (err) {
         console.log(err);
